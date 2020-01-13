268b3db6e3cd38bc7bdd43ed3598c51555759439
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-13 13:43:17 +0800

    更新代码

--- src/main/java/study/springboot/security/jwt/auth/filter/JwtAuthenticationTokenFilter.java
+++ src/main/java/study/springboot/security/jwt/auth/filter/JwtAuthenticationTokenFilter.java
@@ -1,7 +1,11 @@
 package study.springboot.security.jwt.auth.filter;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.buf.StringUtils;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,17 +24,14 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
                                     FilterChain chain) throws ServletException, IOException {
         String authToken = request.getHeader(this.token_header);
         final String auth_token_start = "Bearer ";
-        if (StringUtils.isNotEmpty(auth_token) && auth_token.startsWith(auth_token_start)) {
-            auth_token = auth_token.substring(auth_token_start.length());
+        if (Strings.isNotEmpty(authToken) && authToken.startsWith(auth_token_start)) {
+            authToken = authToken.substring(auth_token_start.length());
         } else {
             // 不按规范,不允许通过验证
-            auth_token = null;
+            authToken = null;
         }
-
         String username = jwtUtils.getUsernameFromToken(auth_token);
-
         logger.info(String.format("Checking authentication for user %s.", username));
-
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             User user = jwtUtils.getUserFromToken(auth_token);
             if (jwtUtils.validateToken(auth_token, user)) {
