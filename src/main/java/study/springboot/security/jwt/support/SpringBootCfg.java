4afa5c8dd9d51abcca0263339b8079a4c2ecbcc2
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-13 13:22:27 +0800

    更新代码

---
+++ src/main/java/study/springboot/security/jwt/support/SpringBootCfg.java
@@ -0,0 +1,7 @@
+package study.springboot.security.jwt.support;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "study.springboot.security.jwt")
+public class SpringBootCfg {
+}
