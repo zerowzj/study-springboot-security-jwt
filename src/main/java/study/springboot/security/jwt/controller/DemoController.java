95bfd4ba4653c0f6c39db95f911545888feb81ab
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-14 09:57:53 +0800

    更新代码

---
+++ src/main/java/study/springboot/security/jwt/controller/DemoController.java
@@ -0,0 +1,13 @@
+package study.springboot.security.jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "demo";
+    }
+}
