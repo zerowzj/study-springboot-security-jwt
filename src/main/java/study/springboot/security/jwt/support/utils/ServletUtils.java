8ef5247e754ffe4c9b03067fe5a4df02d8c7c61b
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-14 11:27:25 +0800

    更新代码

---
+++ src/main/java/study/springboot/security/jwt/support/utils/ServletUtils.java
@@ -0,0 +1,34 @@
+package study.springboot.security.jwt.support.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.util.Map;
+
+@Slf4j
+public class ServletUtils {
+
+    public static void sendError(HttpServletResponse response, int statusCode) {
+        sendError(response, statusCode, null);
+    }
+
+    public static void sendError(HttpServletResponse response, int statusCode, String msg) {
+        try {
+            response.sendError(statusCode, msg);
+        } catch (Exception ex) {
+            log.error("", ex);
+        }
+    }
+
+    public static void write(HttpServletResponse response, Map<String, Object> result) {
+        PrintWriter writer;
+        try {
+            writer = response.getWriter();
+            String text = JsonUtils.toJson(result);
+            writer.write(text);
+        } catch (Exception ex) {
+            log.error("", ex);
+        }
+    }
+}
