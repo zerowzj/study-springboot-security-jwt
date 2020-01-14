9cf22c83dbd028bd9ef808bd8c10bbf2ba7ede72
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-14 16:50:35 +0800

    更新代码

--- src/main/java/study/springboot/security/jwt/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/study/springboot/security/jwt/auth/filter/JwtAuthenticationFilter.java
@@ -1,14 +1,11 @@
 package study.springboot.security.jwt.auth.filter;
 
-import com.sun.xml.internal.ws.handler.ServerLogicalHandlerTube;
 import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import study.springboot.security.jwt.support.Results;
-import study.springboot.security.jwt.support.utils.ServletUtils;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -18,7 +15,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 /**
- * Token的校验
+ * token的校验
  * 该类继承自BasicAuthenticationFilter，在doFilterInternal方法中，
  * 从http头的Authorization 项读取token数据，然后用Jwts包提供的方法校验token的合法性。
  * 如果校验通过，就认为这是一个取得授权的合法请求
@@ -36,7 +33,6 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
         log.info("======> doFilterInternal");
         String header = request.getHeader("Authorization");
         if (header == null || !header.startsWith("Bearer ")) {
-//            ServletUtils.write(response, Results.error("9090", "请先登录"));
             throw new RuntimeException("需要认证");
         }
         UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
