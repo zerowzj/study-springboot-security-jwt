bb8f29d5e81fc08bfe89af2ae4cadb6951e81b7d
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-14 10:44:38 +0800

    更新代码

--- src/main/java/study/springboot/security/jwt/support/utils/JsonUtils.java
+++ src/main/java/study/springboot/security/jwt/support/utils/JsonUtils.java
@@ -8,12 +8,28 @@ import java.io.InputStream;
 @Slf4j
 public class JsonUtils {
 
+    public static String toJson(Object obj) {
+        return JSON.toJSONString(obj);
+    }
+
+    public static <T> T fromJson(String text, Class<T> clazz) {
+        T obj = null;
+        try {
+            obj = JSON.parseObject(text, clazz);
+        } catch (Exception ex) {
+            log.error("", ex);
+            throw ex;
+        }
+        return obj;
+    }
+
     public static <T> T fromJson(InputStream is, Class<T> clazz) {
         T obj = null;
         try {
             obj = JSON.parseObject(is, clazz);
         } catch (Exception ex) {
             log.error("", ex);
+            throw new RuntimeException();
         }
         return obj;
     }
