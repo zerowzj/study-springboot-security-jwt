4975f8b6d7798e1bb7d66d20f78a7d64c9cbfcd3
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-01-14 16:36:28 +0800

    更新代码

--- doc/JwtAuthenticationTokenFilter.java
+++ doc/JwtAuthenticationTokenFilter.java
@@ -17,25 +17,25 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain chain) throws ServletException, IOException {
-//        String token = request.getHeader(this.TOKEN_HEADER);
-//        final String auth_token_start = "Bearer ";
-//        if (Strings.isNotEmpty(token) && token.startsWith(auth_token_start)) {
-//            token = token.substring(auth_token_start.length());
-//        } else {
-//            // 不按规范,不允许通过验证
-//            token = null;
-//        }
-//        String username = jwtUtils.getUsernameFromToken(auth_token);
-//        logger.info(String.format("Checking authentication for user %s.", username));
-//        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//            User user = jwtUtils.getUserFromToken(auth_token);
-//            if (jwtUtils.validateToken(auth_token, user)) {
-//                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-//                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                logger.info(String.format("Authenticated user %s, setting security context", username));
-//                SecurityContextHolder.getContext().setAuthentication(authentication);
-//            }
-//        }
+        String token = request.getHeader(this.TOKEN_HEADER);
+        final String auth_token_start = "Bearer ";
+        if (Strings.isNotEmpty(token) && token.startsWith(auth_token_start)) {
+            token = token.substring(auth_token_start.length());
+        } else {
+            // 不按规范,不允许通过验证
+            token = null;
+        }
+        String username = jwtUtils.getUsernameFromToken(auth_token);
+        logger.info(String.format("Checking authentication for user %s.", username));
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            User user = jwtUtils.getUserFromToken(auth_token);
+            if (jwtUtils.validateToken(auth_token, user)) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                logger.info(String.format("Authenticated user %s, setting security context", username));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
         chain.doFilter(request, response);
     }
 }
